93f3c40cbaad5d40d7ed6770a421f41283574cf2
UnnamedMistress <momchrysti@gmail.com>
2023-03-18 14:48:02 -0400

    update server file

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-// server.js
-
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(port, () => {
