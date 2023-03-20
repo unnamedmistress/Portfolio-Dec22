a61cd85b3e5257dfd86d1bb4528c637768f14600
UnnamedMistress <momchrysti@gmail.com>
2023-03-20 09:48:54 -0400

    testing logs

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const sendEmail = require('./email');
 const app = express();
 
 // Initialize dotenv
